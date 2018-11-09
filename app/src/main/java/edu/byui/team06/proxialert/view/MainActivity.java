d870d3cc446ec2de35545a5b88a54012d019ae45
Chase Busacker <cwbusacker@hotmail.com>
2018-11-08 19:32:23 -0700

    Notification OnCreate Practice

--- app/src/main/java/edu/byui/team06/proxialert/view/MainActivity.java
+++ app/src/main/java/edu/byui/team06/proxialert/view/MainActivity.java
@@ -1,5 +1,14 @@
 package edu.byui.team06.proxialert.view;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -10,10 +19,36 @@ import static java.lang.Math.sqrt;
 
 public class MainActivity extends AppCompatActivity {
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        String channelId = "myNotification";
+        CharSequence channelName = "Some Channel";
+        int importance = NotificationManager.IMPORTANCE_HIGH;
+        NotificationChannel notificationChannel = new NotificationChannel(channelId, channelName, importance);
+        notificationChannel.enableLights(true);
+        notificationChannel.setLightColor(Color.BLUE);
+        notificationChannel.enableVibration(true);
+        notificationChannel.setVibrationPattern(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
+        notificationManager.createNotificationChannel(notificationChannel);
+
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this, "myNotification")
+                .setLargeIcon(BitmapFactory.decodeFile(Context.getFilesDir().getPath("/data/data/edu.byui.team06.proxialert/IMG_2149.JPG"))
+                        .setSmallIcon(R.drawable.)
+                        .setContentTitle("ProxiAlert!")
+                        .setContentText("John is within 1 mile of your current location! Have time to visit?")
+                        .setStyle(new NotificationCompat.BigTextStyle()
+                                .bigText("John is within 1 mile of your current location! Have time to visit?"))
+                        .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+// notificationId is a unique int for each notification that you must define
+        notificationManager.notify(100, mBuilder.build());
     }
 
     static public Boolean getDistance(float x1, float y1, float x2, float y2, float distance)
