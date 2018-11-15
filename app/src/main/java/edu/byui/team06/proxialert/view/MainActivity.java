ab47bb1df402ab70494f669d4930fb7389ffc5fb
DVDet <DVDet@byui.edu>
2018-11-14 20:42:08 -0700

    prepping files for settings menu and shared preferences.

--- app/src/main/java/edu/byui/team06/proxialert/view/MainActivity.java
+++ app/src/main/java/edu/byui/team06/proxialert/view/MainActivity.java
@@ -29,6 +29,7 @@ import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
+import android.view.Menu;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -55,6 +56,13 @@ public class MainActivity extends AppCompatActivity {
     private TextView noTaskView;
 
     private DatabaseHelper db;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
