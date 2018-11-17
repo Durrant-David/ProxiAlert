31c3b5bc377292909d174b5dd2c2c91cf7a6cd3d
Kristina Hayes <khayes9@byui.edu>
2018-11-17 12:40:43 -0700

    Shared Preferences WORKING

--- app/src/main/java/edu/byui/team06/proxialert/view/MainActivity.java
+++ app/src/main/java/edu/byui/team06/proxialert/view/MainActivity.java
@@ -3,6 +3,8 @@ package edu.byui.team06.proxialert.view;
 //notification imports (many could probably be removed
 //since it was moved to its own class
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 
 //database imports
@@ -42,8 +44,30 @@ public class MainActivity extends AppCompatActivity {
     private CoordinatorLayout coordinatorLayout;
     private RecyclerView recyclerView;
     private TextView noTaskView;
-
+    private int SETTINGS_ACTION = 1;
     private DatabaseHelper db;
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.settings:
+                startActivityForResult(new Intent(this,
+                        SettingsActivity.class), SETTINGS_ACTION);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == SETTINGS_ACTION) {
+            if (resultCode == SettingsActivity.RESULT_CODE_THEME_UPDATED) {
+                finish();
+                startActivity(getIntent());
+                return;
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -59,6 +83,17 @@ public class MainActivity extends AppCompatActivity {
     }
 
     protected void onCreate(Bundle savedInstanceState) {
+        SharedPreferences pref = PreferenceManager
+                .getDefaultSharedPreferences(this);
+        boolean themeName = pref.getBoolean("themes", false);
+        if (themeName) {
+            setTheme(R.style.ThemeOverlay_MaterialComponents_Dark);
+        } else {
+            Toast.makeText(this, "set theme", Toast.LENGTH_SHORT).show();
+            setTheme(R.style.AppTheme);
+        }
+        Toast.makeText(this, "Theme has been reset to " + themeName,
+                Toast.LENGTH_SHORT).show();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
--- app/src/main/java/edu/byui/team06/proxialert/view/SettingsActivity.java
+++ app/src/main/java/edu/byui/team06/proxialert/view/SettingsActivity.java
@@ -1,5 +1,29 @@
 package edu.byui.team06.proxialert.view;
+import android.os.Bundle;
+import android.preference.Preference;
+import edu.byui.team06.proxialert.R;
 
 public class SettingsActivity extends AppCompatPreferenceActivity {
+    public static final int RESULT_CODE_THEME_UPDATED = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.preferences);
+//      findPreference("theme").setOnPreferenceChangeListener(new RefershActivityOnPreferenceChangeListener(RESULT_CODE_THEME_UPDATED));
+//    }
+//
+//    private class RefershActivityOnPreferenceChangeListener implements Preference.OnPreferenceChangeListener {
+//        private final int resultCode;
+//        public RefershActivityOnPreferenceChangeListener(int resultCode) {
+//            this.resultCode = resultCode;
+//        }
+//
+//        @Override
+//        public boolean onPreferenceChange(Preference p, Object newValue) {
+//            setResult(resultCode);
+//            return true;
+//        }
+    }
 
 }
