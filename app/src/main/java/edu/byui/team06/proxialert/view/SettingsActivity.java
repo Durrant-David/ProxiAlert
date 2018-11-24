329e55dd57d15e4c3fc9f0d3186abdd83eb92f9d
Kristina Hayes <khayes9@byui.edu>
2018-11-23 19:58:52 -0700

    Shared Preferences WORKING

--- app/src/main/java/edu/byui/team06/proxialert/view/SettingsActivity.java
+++ app/src/main/java/edu/byui/team06/proxialert/view/SettingsActivity.java
@@ -10,20 +10,20 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.preferences);
-//      findPreference("theme").setOnPreferenceChangeListener(new RefershActivityOnPreferenceChangeListener(RESULT_CODE_THEME_UPDATED));
-//    }
-//
-//    private class RefershActivityOnPreferenceChangeListener implements Preference.OnPreferenceChangeListener {
-//        private final int resultCode;
-//        public RefershActivityOnPreferenceChangeListener(int resultCode) {
-//            this.resultCode = resultCode;
-//        }
-//
-//        @Override
-//        public boolean onPreferenceChange(Preference p, Object newValue) {
-//            setResult(resultCode);
-//            return true;
-//        }
+        findPreference("themes").setOnPreferenceChangeListener(new RefreshActivityOnPreferenceChangeListener(RESULT_CODE_THEME_UPDATED));
+     }
+// TODO: Fix bugs with Theme Change
+      private class RefreshActivityOnPreferenceChangeListener implements Preference.OnPreferenceChangeListener { private final int resultCode;
+      public RefreshActivityOnPreferenceChangeListener(int resultCode) {
+          this.resultCode = resultCode;
+
+      }
+
+        @Override
+        public boolean onPreferenceChange(Preference p, Object newValue) {
+            setResult(resultCode);
+            return true;
+        }
     }
 
 }
